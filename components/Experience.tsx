8b96ecda679ea427de54fa3865c96502d1a4007e
uzairkhan-12 <pk.uzikhan@gmail.com>
2025-04-04 08:34:15 +0300

    fixed experience cards issue on mobile view

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,36 +10,36 @@ import "react-vertical-timeline-component/style.min.css";
 
 const Experience: React.FC = () => {
   return (
-    <div id="experience" className="mt-8 md:mt-32">
+    <div id="experience" className="mt-8 md:mt-32 overflow-hidden">
       <h2 className="text-4xl mb-5 md:mb-10 font-bold text-center text-primary">
         Experience
       </h2>
-      <VerticalTimeline>
-        {timeline.map((t, i) => {
-          const contentStyle = { background: "#1F2937", color: "#fff" };
-          return (
-            <VerticalTimelineElement
-              key={i}
-              className="vertical-timeline-element--work"
-              contentStyle={contentStyle}
-              date={t.date}
-              {...t.icon}
-            >
-              {t.title ? (
-                <React.Fragment>
-                  <h3 className="vertical-timeline-element-title">{t.title}</h3>
-                  {t.subtitle && (
-                    <h4 className="vertical-timeline-element-subtitle">
-                      {t.subtitle}
-                    </h4>
-                  )}
-                  {t.desc && <p>{t.desc}</p>}
-                </React.Fragment>
-              ) : undefined}
-            </VerticalTimelineElement>
-          );
-        })}
-      </VerticalTimeline>
+        <VerticalTimeline>
+          {timeline.map((t, i) => {
+            const contentStyle = { background: "#1F2937", color: "#fff" };
+            return (
+              <VerticalTimelineElement
+                key={i}
+                className="vertical-timeline-element--work"
+                contentStyle={contentStyle}
+                date={t.date}
+                {...t.icon}
+              >
+                {t.title ? (
+                  <React.Fragment>
+                    <h3 className="vertical-timeline-element-title">{t.title}</h3>
+                    {t.subtitle && (
+                      <h4 className="vertical-timeline-element-subtitle">
+                        {t.subtitle}
+                      </h4>
+                    )}
+                    {t.desc && <p>{t.desc}</p>}
+                  </React.Fragment>
+                ) : undefined}
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
     </div>
   );
 };
